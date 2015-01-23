55e288b4bde921c06d97634246427f3d46e6b782
bhaddad5 <bhaddad5@gmail.com>
2015-01-22 23:27:28 -0800

    Crazy Map Generation

--- LaaK/Assets/Scripts/GameManager.js
+++ LaaK/Assets/Scripts/GameManager.js
@@ -1,21 +1,36 @@
-﻿var clicked = 0;
-var colorMap : Texture2D;
-var map : Map;
+﻿var map : Map;
 var army : GameObject;
+var regions_map : Texture2D;
+var terrainMap : Texture2D;
+var map_base : Texture2D;
+var terrain_base : Texture2D;
+var tiles : Texture2D;
+var treeTiles : Texture2D;
+var clicked = 0;
 
 function Start () {
 	Random.seed = Time.time;
 	
-	map = new Map();
+	map = new Map(regions_map, terrainMap, map_base, terrain_base, tiles, treeTiles);
 }
 
 function Update () {
+
+	UpdateCameraPos();
+	
+	if (Input.GetKeyDown ("r")){
+		//var clearTile = regions_map.GetPixels(0,0,20,20,0);
+	}
+
 	//Only proceed if clicking down once
 	if (Input.GetMouseButton (0) && clicked==0){
 		var region = getClickedRegion();
 		
-		Debug.Log(region.name);
-		spawnArmy(region);
+		//spawnArmy (region);
+		Debug.Log(region.name+" borders:");
+		for(var i = 0; i<region.borders.length; i++){
+			Debug.Log("     "+region.borders[i].name);
+		}
 		
 		clicked = 1;
 	}
@@ -25,57 +40,50 @@ function Update () {
 	}
 }
 
+function UpdateCameraPos () {
+	var mouseX = Input.mousePosition.x;
+	var mouseY = Input.mousePosition.y;
+	var scrollArea = 20;
+	var camTransform = Camera.main.transform;
+
+	var camLeft = Camera.main.ScreenToWorldPoint(Vector3(0,0,0)).x;
+	var camRight = Camera.main.ScreenToWorldPoint(Vector3(Camera.main.pixelWidth,0,0)).x;
+	var camTop = Camera.main.ScreenToWorldPoint(Vector3(0,Camera.main.pixelHeight,0)).y;
+	var camBot = Camera.main.ScreenToWorldPoint(Vector3(0,0,0)).y;
+	
+	if(mouseX <= scrollArea && mouseX >= 0 && Mathf.Abs(camLeft) <= (map_base.width/2)/100f){
+		camTransform.Translate(-.1,0,0);
+	}
+	if(mouseX >= (Screen.width - scrollArea) && mouseX <= Screen.width && Mathf.Abs(camRight) <= (map_base.width/2)/100f){
+		camTransform.Translate(.1,0,0);
+	}
+	
+	if(mouseY >= (Screen.height - scrollArea) && mouseY <= Screen.height && Mathf.Abs(camTop) <= (map_base.height/2)/100f){
+		camTransform.Translate(0,0.1,0);
+	}
+	if(mouseY <= scrollArea && mouseY >= 0 && Mathf.Abs(camBot) <= (map_base.height/2)/100f){
+		camTransform.Translate(0,-0.1,0);
+	}
+}
+
 function spawnArmy (r:Region) {
-/*	var mapWidth:float = parseFloat(map.manualWidth);
-	var mapHeight:float = parseFloat(map.manualHeight);
-	var mapOffsetX:float = parseFloat(mapWidth/2);
-	var mapOffsetY:float = parseFloat(mapHeight/2);
-	var posX:float = parseFloat(r.position.x)/parseFloat(colorMap.width);
-	var posY:float = parseFloat(r.position.y)/parseFloat(colorMap.height);
-	
-	var adjustedX:float = parseFloat(posX)*parseFloat(mapWidth) - parseFloat(mapOffsetX);
-	var adjustedY:float = parseFloat(posY)*parseFloat(mapWidth) - parseFloat(mapOffsetY);*/
-	
 	var spawnPosition : Vector3 = new Vector3 (r.position.x, r.position.y, 0);
 	Instantiate (army, spawnPosition, Quaternion.identity);
 }
 
-function getRegionByColor(c:Color32) {
-	for(var i = 0; i<map.regions.length; i++){
-		if(map.regions[i].color == c){
-			return map.regions[i];
-		}
-	}
-	return null;
-}
-
 function getClickedRegion () {
 	var mousePos : Vector2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-	var mapPosX : float = (mousePos.x*100)+(colorMap.width/2);
-	var mapPosY : float = (mousePos.y*100)+(colorMap.height/2);
-	var mapPos : Vector2 = new Vector2(mapPosX, mapPosY);
-	var clickedColor : Color32 = colorMap.GetPixel(mapPos.x, mapPos.y);
-	
-	Debug.Log(mousePos);
-	
+	var mapPosX : float = (mousePos.x*100)+(map_base.width/2);
+	var mapPosY : float = (mousePos.y*100)+(map_base.height/2);
+	var mapPos : Vector2 = new Vector2(mapPosX/40, mapPosY/40);
+	var clickedColor : Color32 = regions_map.GetPixel(mapPos.x, mapPos.y);
+
 	for(var i = 0; i<map.regions.length; i++){
-		if(compareColors(clickedColor, map.regions[i].color)){
+		if(clickedColor == map.regions[i].color){
 			return map.regions[i];
 		}
 	}
-	
 	return map.regions[0];
-}
-
-function compareColors (clickedColor:Color32, regionColor:Color32) {
-	var redEquals:boolean = (clickedColor.r == regionColor.r);
-	var greenEquals:boolean = (clickedColor.g == regionColor.g);
-	var blueEquals:boolean = (clickedColor.b == regionColor.b);
-    
-    if(!redEquals||!greenEquals||!blueEquals){
-    	return false;
-    }
-    else return true;
 }
 
 
--- LaaK/Assets/Scripts/Map.js
+++ LaaK/Assets/Scripts/Map.js
@@ -1,16 +1,33 @@
-﻿class Map{
+﻿class Map {
 	var regions = new Array();
-	var manualWidth = 10;
-	var manualHeight = 10;
-	
-	function Map(){
+	var regionsMap : Texture2D;
+	var terrainMap : Texture2D;
+	var base : Texture2D;
+	var terrain_base : Texture2D;
+	var tiles : Texture2D;
+	var treeTiles : Texture2D;
+	var tSize = 40;
+		
+	function Map(rMap : Texture2D, tMap : Texture2D, baseMap : Texture2D, baseTerrain : Texture2D, tileTexturs : Texture2D, trees : Texture2D){
+		regionsMap = rMap;
+		terrainMap = tMap;
+		base = baseMap;
+		terrain_base = baseTerrain;
+		tiles = tileTexturs;
+		treeTiles = trees;
 		generateRegions();
 	}
 	
-	function generateRegions() {
+function generateRegions() {
+	// This must always be the region occupying the top left corner
+	// of the map for generateBorders to work.
 	var Ocean = new Region("Ocean", "ocean", Color32(0,0,255,255));
 		Ocean.position = new Vector2(0,0);
 		regions.Push(Ocean);
+		
+	var Mountains = new Region("Mountains", "ocean", Color32(0,0,0,255));
+		Mountains.position = new Vector2(0,0);
+		regions.Push(Mountains);
 	
 	var Silvergate = new Region("Silvergate", "plains", Color32(155,33,33,255));
 		Silvergate.position = new Vector2(-3.4,2.5);
@@ -48,14 +65,238 @@
 		ValeSur.position = new Vector2(4.3,2.3);
 		regions.Push(ValeSur);
 		
-	Silvergate.borders.Push(Darrenport, MilesLanding);
-	Darrenport.borders.Push(Silvergate, Esterwood, Wraithden);
-	Wraithden.borders.Push(Sansford, Esterwood, Darrenport);
-	MilesLanding.borders.Push(Sansford, Darrenport, Sansmarsh);
-	Sansford.borders.Push(Wraithden, MilesLanding, Sansmarsh);
-	Sansmarsh.borders.Push(Sansford, MilesLanding);
-	Esterwood.borders.Push(Wraithden, ValeSur, HighGates, Darrenport);
-	ValeSur.borders.Push(Esterwood);
-	HighGates.borders.Push(Esterwood);
-	}
+	generateBorders();
+	
+	spawnTiles();
 }
+
+function generateBorders(){
+	var prevColor : Color32 = Color32(0,0,255,255);
+	var currColor : Color32;
+	var prevRegion : Region = regions[0];
+	var currRegion : Region = regions[0];
+	
+	//First run them all side to side
+	for(var i = 0; i<regionsMap.height; i++){
+		for(var j = 0; j<regionsMap.width; j++){
+			currColor = regionsMap.GetPixel(j,i);
+						
+			if(currColor != prevColor){
+				prevColor = currColor;
+				prevRegion = currRegion;
+				currRegion = getRegionByColor(currColor);
+				insertBorder(prevRegion, currRegion);
+				insertBorder(currRegion, prevRegion);
+				checkTop = true;
+			}
+			else prevColor = currColor;
+		}		
+	}
+	
+	//Now do it again top to bottom
+	for(var a = 0; a<regionsMap.width; a++){
+		for(var b = 0; b<regionsMap.height; b++){
+			currColor = regionsMap.GetPixel(a,b);
+			
+			if(currColor != prevColor){
+				prevColor = currColor;
+				prevRegion = currRegion;
+				currRegion = getRegionByColor(currColor);
+				insertBorder(prevRegion, currRegion);
+				insertBorder(currRegion, prevRegion);
+			}
+			else prevColor = currColor;
+		}		
+	}
+	
+}
+
+function getRegionByColor(c:Color32) {
+	for(var i = 0; i<regions.length; i++){
+		if(regions[i].color == c){
+			return regions[i];
+		}
+	}
+	return null;
+}
+
+function insertBorder(main : Region, adding : Region) {
+	if(main == regions[0] || main == regions[1] || adding == regions[0] || adding == regions[1])
+		return;
+
+	for(var i = 0; i<main.borders.length; i++){
+		if(main.borders[i] == adding){
+			return;
+		}
+	}
+	main.borders.Push(adding);	
+}
+
+function spawnTiles(){
+	var oceanColor = new Color32(0,0,255,255);
+	var forestColor = new Color32(0,255,0,255);
+	var mountainColor = new Color32(255,255,255,255);
+	
+	var coastArray1 = buildTileArray(tSize,2, tiles);
+	var coastArray2 = buildTileArray(tSize*5,2, tiles);
+	var coastArray3 = buildTileArray(tSize*9,2, tiles);
+	var coastArray4 = buildTileArray(tSize*13,2, tiles);
+	var coastArray5 = buildTileArray(tSize*17,1, tiles);
+	
+	var forestArray = buildAssetArray(0,3,treeTiles);
+	
+	var landTile = tiles.GetPixels(0,0,tSize,tSize,0);
+	var waterTile = tiles.GetPixels(tSize,0,tSize,tSize,0);
+	
+	//Loop through every square on the RegionsMap, draw the coast and the terrain
+	//TODO: Clamp texture to prevent overflow?
+	for(var i = 0; i<regionsMap.width; i++){
+		for(var j = regionsMap.height - 1; j>=0; j--){
+		
+			var pos = new Vector2(i,j);
+		
+			tileColor = regionsMap.GetPixel(i,j);
+			leftColor = regionsMap.GetPixel(i-1,j);
+			rightColor = regionsMap.GetPixel(i+1,j);
+			topColor = regionsMap.GetPixel(i,j+1);
+			botColor = regionsMap.GetPixel(i,j-1);
+			
+		if(tileColor != oceanColor){
+			//checkAdjacentColors(left, right, top, bottom, etc... checks if each true side is the color it is checking
+			
+			if(checkAdjacentColors(false, false, true, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray1[Random.Range(0,coastArray1.length)])[3],0);
+			}
+			else if(checkAdjacentColors(false, false, false, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray1[Random.Range(0,coastArray1.length)])[1],0);
+			}
+			else if(checkAdjacentColors(true, false, false, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray1[Random.Range(0,coastArray1.length)])[2],0);
+			}
+			else if(checkAdjacentColors(false, true, false, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray1[Random.Range(0,coastArray1.length)])[0],0);
+			}
+			
+			else if(checkAdjacentColors(false, true, true, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray2[Random.Range(0,coastArray2.length)])[0],0);
+			}
+			else if(checkAdjacentColors(true, false, true, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray2[Random.Range(0,coastArray2.length)])[3],0);
+			}
+			else if(checkAdjacentColors(false, true, false, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray2[Random.Range(0,coastArray2.length)])[1],0);
+			}
+			else if(checkAdjacentColors(true, false, false, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray2[Random.Range(0,coastArray2.length)])[2],0);
+			}
+			
+			else if(checkAdjacentColors(false, false, true, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray3[Random.Range(0,coastArray3.length)])[1],0);
+			}
+			else if(checkAdjacentColors(true, true, false, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray3[Random.Range(0,coastArray3.length)])[0],0);
+			}
+			
+			else if(checkAdjacentColors(true, true, false, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray4[Random.Range(0,coastArray4.length)])[2],0);
+			}
+			else if(checkAdjacentColors(true, true, true, false, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray4[Random.Range(0,coastArray4.length)])[0],0);
+			}
+			else if(checkAdjacentColors(false, true, true, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray4[Random.Range(0,coastArray4.length)])[3],0);
+			}
+			else if(checkAdjacentColors(true, false, true, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray4[Random.Range(0,coastArray4.length)])[1],0);
+			}
+			
+			else if(checkAdjacentColors(true, true, true, true, pos, oceanColor)){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,(coastArray5[Random.Range(0,coastArray5.length)])[0],0);
+			}
+			
+			else if(tileColor != oceanColor){
+				base.SetPixels(i*tSize,j*tSize,tSize,tSize,landTile,0);
+			}
+		}
+		else base.SetPixels(i*tSize,j*tSize,tSize,tSize,waterTile,0);
+		
+		//Set Terrain
+		if(terrainMap.GetPixel(i,j) == forestColor){
+			//base.SetPixels(i*tSize,j*tSize,tSize,tSize,forestArray[0],0);
+			terrain_base.SetPixels(i*tSize,j*tSize,tSize,tSize,forestArray[0],0);
+			Debug.Log(terrain_base.GetPixel(i*tSize+20,j*tSize+20));
+		}
+		
+		}		
+	}
+	
+	base.Apply();
+}
+
+function checkAdjacentColors(left : boolean, right : boolean, top : boolean, bot : boolean, position : Vector2, color : Color32){
+    tileColor = regionsMap.GetPixel(position.x, position.y);
+	leftColor = regionsMap.GetPixel(position.x-1, position.y);
+	rightColor = regionsMap.GetPixel(position.x+1, position.y);
+	topColor = regionsMap.GetPixel(position.x, position.y+1);
+	botColor = regionsMap.GetPixel(position.x, position.y-1);
+	
+	if(left == true && leftColor != color)
+			return false;
+	if(left == false && leftColor == color)
+			return false;
+	
+	if(right == true && rightColor != color)
+			return false;
+	if(right == false && rightColor == color)
+			return false;
+	
+	if(top == true && topColor != color)
+			return false;
+	if(top == false && topColor == color)
+			return false;
+	
+	if(bot == true && botColor != color)
+			return false;
+	if(bot == false && botColor == color)
+			return false;
+	
+	return true;
+}
+
+//Build Array of Arrays of tiles.  The inside arrays contain all 4 rotations of each tile
+
+function buildTileArray(height : int, num : int, sourceTiles : Texture2D){
+	var tileArray = new Array();
+	var rotationArray;
+		
+	for(var i = 0; i<num; i++){
+		rotationArray = new Array();
+		rotationArray.Push(sourceTiles.GetPixels(tSize*i, height, tSize,tSize,0));
+		rotationArray.Push(sourceTiles.GetPixels(tSize*i, height+tSize, tSize,tSize,0));
+		rotationArray.Push(sourceTiles.GetPixels(tSize*i, height+tSize*2, tSize,tSize,0));
+		rotationArray.Push(sourceTiles.GetPixels(tSize*i, height+tSize*3, tSize, tSize,0));
+		
+		tileArray.Push(rotationArray);
+	}
+	
+	return tileArray;
+}
+
+function buildAssetArray(height : int, num : int, sourceTiles : Texture2D){
+	var assetArray = new Array();
+		
+	for(var i = 0; i<num; i++){
+		assetArray.Push(sourceTiles.GetPixels(tSize*i, height, tSize,tSize,0));
+	}
+	
+	return assetArray;
+}
+}
+
+
+
+
+
+
+
+
